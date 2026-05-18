79eb5a49075a5f8a79bc38a0e3fe4fabc11cf093
Simon Forsberg <simon.forsberg@gu.se>
2026-05-18 13:56:45 +0200

    Baby steps

--- web/src/avatar-models.js
+++ web/src/avatar-models.js
@@ -1,11 +1,28 @@
 import raccoonHeadModel from "./avatars/raccoon_head_small.glb?url";
 import rpmAvatar from "./avatars/rpm_avatar.glb?url";
-import babyModel from "./avatars/baby-shapes.glb?url";
+import babyModel from "./avatars/baby.glb?url";
+import babyModelLower from "./avatars/babylower.glb?url";
+import babyModel2 from "./avatars/Untitled.glb?url";
 
 export const modelOptions = [
   {
-    name: "Baby",
-    model: babyModel,
+    name: "BabyLow",
+    model: babyModelLower,
+    cameraConfig: {
+      target: [0, 3, -1.2],
+    },
+    modelOptions: {
+      scale: 0.4,
+    }
+  },
+  {
+    name: "Baby2",
+    model: babyModel2,
+    cameraConfig: {
+      fov: 60,
+      position: [0, -3, 6.2],
+      target: [0, 0, 0]
+    },
     modelOptions: {
       scale: 0.4,
     }
