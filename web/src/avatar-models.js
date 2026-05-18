c3a287855a47e4059c24a9ada62cd5e72a746214
Simon Forsberg <simon.forsberg@gu.se>
2026-05-18 15:02:47 +0200

    Improve baby, add Zhenja

--- web/src/avatar-models.js
+++ web/src/avatar-models.js
@@ -1,15 +1,21 @@
 import raccoonHeadModel from "./avatars/raccoon_head_small.glb?url";
 import rpmAvatar from "./avatars/rpm_avatar.glb?url";
 import babyModel from "./avatars/baby.glb?url";
-import babyModelLower from "./avatars/babylower.glb?url";
 import babyModel2 from "./avatars/Untitled.glb?url";
+import zhenja from "./avatars/zhenja.glb?url";
 
 export const modelOptions = [
   {
-    name: "BabyLow",
-    model: babyModelLower,
+    name: "Babyglb",
+    model: babyModel,
     cameraConfig: {
-      target: [0, 3, -1.2],
+      fov: 60,
+      position: [0, 0, 6.2],
+      target: [0, 0, 0],
+    },
+    blendshapeMultipliers: {
+      browOuterUpLeft: 2.0,
+      browOuterUpRight: 2.0,
     },
     modelOptions: {
       scale: 0.4,
@@ -25,6 +31,21 @@ export const modelOptions = [
     },
     modelOptions: {
       scale: 0.4,
+    }
+  },
+  {
+    name: "Zhenja",
+    model: zhenja,
+    cameraConfig: {
+      fov: 60,
+      position: [0, 1.65, 1.2],
+      target: [0, 1.6, 0]
+    },
+    modelOptions: {
+      center: true,
+      autoRotate: false,
+      scale: 1.8,
+      fullBodyAvatar: true
     }
   },
   {
