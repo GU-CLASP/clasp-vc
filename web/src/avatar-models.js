1d0dd1e8752c22ae3a9fc78494ba05e240fa6166
Simon Forsberg <simon.forsberg@gu.se>
2026-05-18 15:36:49 +0200

    Add more avatars

--- web/src/avatar-models.js
+++ web/src/avatar-models.js
@@ -2,9 +2,90 @@ import raccoonHeadModel from "./avatars/raccoon_head_small.glb?url";
 import rpmAvatar from "./avatars/rpm_avatar.glb?url";
 import babyModel from "./avatars/baby.glb?url";
 import babyModel2 from "./avatars/Untitled.glb?url";
+import andra from "./avatars/andra.glb?url";
+import ashima from "./avatars/ashima.glb?url";
+import cyborg from "./avatars/cyborg.glb?url";
+import fireLady from "./avatars/fire-lady.glb?url";
+import harry from "./avatars/harry.glb?url";
 import zhenja from "./avatars/zhenja.glb?url";
 
 export const modelOptions = [
+  // [x, y, z] is: left-right (horizontal), up-down (vertical), faraway-close (depth)
+  {
+    name: "Andra",
+    model: andra,
+    cameraConfig: {
+      fov: 60,
+      position: [0, 1.65, 1.2],
+      target: [0, 1.6, 0]
+    },
+    modelOptions: {
+      center: true,
+      autoRotate: false,
+      scale: 1.8,
+      fullBodyAvatar: true
+    }
+  },
+  {
+    name: "Ashima",
+    model: ashima,
+    cameraConfig: {
+      fov: 60,
+      position: [0, 1.65, 1.2],
+      target: [0, 1.6, 0]
+    },
+    modelOptions: {
+      center: true,
+      autoRotate: false,
+      scale: 1.8,
+      fullBodyAvatar: true
+    }
+  },
+  {
+    name: "FireLady",
+    model: fireLady,
+    cameraConfig: {
+      fov: 60,
+      position: [0, 1.8, 1.2],
+      target: [0, 1.75, 0]
+    },
+    modelOptions: {
+      center: true,
+      autoRotate: false,
+      scale: 1.8,
+      fullBodyAvatar: true
+    }
+  },
+  {
+    name: "Cyborg",
+    model: cyborg,
+    cameraConfig: {
+      fov: 60,
+      position: [0, 2.0, 1.2],
+      target: [0, 1.95, 0]
+    },
+    modelOptions: {
+      center: true,
+      autoRotate: false,
+      scale: 1.8,
+      fullBodyAvatar: true
+    }
+  },
+  {
+    name: "Harry Potter",
+    model: harry,
+    cameraConfig: {
+      fov: 60,
+      position: [0, 1.9, 1.2],
+      target: [0, 1.9, 0]
+    },
+    modelOptions: {
+      center: true,
+      autoRotate: false,
+      scale: 1.8,
+      fullBodyAvatar: true
+    }
+  },
   {
     name: "Babyglb",
     model: babyModel,
